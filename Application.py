d6aefd65e976768db23cb2081ae8ad2592c531b5
Karun Kanda <kkanda900@gmail.com>
2021-07-06 12:06:00 -0400

    update

---
+++ Application.py
@@ -0,0 +1,55 @@
+# Typical Python Libraries
+import cv2, os
+import numpy as np
+from PIL import Image
+
+# Python GUI
+import PySimpleGUI as sg
+
+# Multi Image Classifier Library
+from Multi_Classification.Multi_Image_Classification import Multi_Image_Classification as img_classifier
+
+# Binary Image Classifier Library
+from Binary_Classification.Image_Classification import Image_Classification as bin_classifier
+
+class Application:
+
+    def run_application(self):
+        # define the window layout => video, image, classification on the bottom
+        layout = [[sg.Image(filename='', key='_IMAGE_'), sg.Image(r'', key='IMAGE')], [sg.Text(
+            'Classification: ', key='label', font='Courier 9', size=(50, 1))], [sg.Button('Take a Picture'), sg.Button('Process Image')]]
+
+        # create the window and show it without the plot
+        window = sg.Window('Pollution Detector', layout, location=(100, 100))
+
+        # --- Event LOOP Read and display frames, operate the GUI --- #
+        # Setup the OpenCV capture device (webcam)
+        cap = cv2.VideoCapture(0)
+        # iterate through infinitely until the user closes the application
+        while True:
+            # define these values for when there are certain events that occur in the application
+            event, values = window.Read(timeout=20, timeout_key='timeout')
+
+            # if there is no more events the application isn't running
+            if event is None:
+                break
+
+            # if the user clicks the 'Take a Picture' button clear the directory then take a picture
+            if event == 'Take a Picture':
+                cv2.imwrite('./App_Data/file.png', frame) # write the image to the directory
+                img = cv2.imencode('.png', np.float32(Image.open("./App_Data/file.png")))[1].tobytes() # to display the image in the GUI convert it to float32
+                window.FindElement('IMAGE').Update(data=img) # replaces the image and add it to the GUI according to the key
+
+            # if the user clicks 'Process Image' button then classify the image that was just taken
+            if event == 'Process Image':
+                print("WHICH MODEL HOE")
+
+            # Read image from capture device (camera)
+            ret, frame = cap.read()
+            # Convert the image to PNG Bytes
+            imgbytes = cv2.imencode('.png', frame)[1].tobytes()
+            # Change the Image Element to show the new image
+            window.FindElement('_IMAGE_').Update(data=imgbytes)
+
+app = Application()
+app.run_application()
---
+++ README.md
@@ -0,0 +1,1 @@
+# UW-Pollution-Detector
